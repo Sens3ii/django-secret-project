06462018720eefef937a1db8b6ba3ebb77e5fd44
Yernur <isenbaev121102ernur@gmail.com>
2023-04-16 03:23:02 +0600

    implemented business logic

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,10 +10,6 @@ class Sale(models.Model):
     total_net_profit = models.BigIntegerField(default=0)
     total_revenue = models.BigIntegerField(default=0)
     total_quantity = models.BigIntegerField(default=0)
-    first_matched_supply = models.ForeignKey('Supply', on_delete=models.DO_NOTHING, null=True, related_name='xx')
-    fms_available_quantity = models.IntegerField(null=True)
-    last_matched_supply = models.ForeignKey('Supply', on_delete=models.DO_NOTHING, null=True, related_name='yy')
-    lms_remaining_quantity = models.IntegerField(null=True)
 
     class Meta:
         indexes = [
@@ -28,12 +24,9 @@ class Supply(models.Model):
     quantity = models.IntegerField(default=1)
     price = models.IntegerField(default=0)
     supply_time = models.DateTimeField()
-    first_matched_sale_id = models.IntegerField(null=True)
 
     class Meta:
         indexes = [
             models.Index(fields=['barcode', 'supply_time', 'id']),
         ]
         db_table = 'umag_hacknu_supply'
-
-
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,24 +1,35 @@
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from itertools import chain
-
-from .serializers import SaleSerializer, SupplySerializer, SaleUpdateSerializer, SupplyUpdateSerializer
-from django.db.models import Sum, Min, Max, Q
+from django.db.models import Max
 from django.http import JsonResponse
 from django.utils import timezone
+
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Sale, Supply
-from rest_framework import generics, mixins, views
+from .serializers import SaleSerializer, SupplySerializer, SaleUpdateSerializer, SupplyUpdateSerializer
 
 
-def recalculate(sales, supplies, prev_sale, fm, fmq):
+def make_kwargs(barcode, from_time, to_time, is_sale):
+    kwargs = {}
+    if from_time:
+        from_time = timezone.datetime.strptime(from_time, '%Y-%m-%d %H:%M:%S')
+        kwargs['sale_time__gte' if is_sale else 'supply_time__gte'] = from_time
+    if to_time:
+        to_time = timezone.datetime.strptime(to_time, '%Y-%m-%d %H:%M:%S')
+        kwargs['sale_time__lte' if is_sale else 'supply_time__lte'] = to_time
+    if barcode:
+        kwargs['barcode'] = barcode
+    return kwargs
+
+
+def recalculate(sales, supplies, fm, fmq):
     upd_sales = []
-    supply_avail_q = None
+    supply = None
+    supply_avail_q = 0
+    prev_sale = None
     for sale in sales:
-        upd_supplies = []
         matched = False
-        first_supply_matched = False
         sale.total_net_profit = prev_sale.total_net_profit if prev_sale else 0
         sale.total_quantity = prev_sale.total_quantity if prev_sale else 0
         sale.total_revenue = prev_sale.total_revenue if prev_sale else 0
@@ -27,33 +38,17 @@ def recalculate(sales, supplies, prev_sale, fm, fmq):
             try:
                 if not supply_avail_q:
                     supply = next(supplies)
-                    if supply_avail_q == 0:
-                        supply.first_matched_sale_id = None
+                    supply_avail_q = supply.quantity
             except StopIteration:
                 break
-            supply_avail_q = supply.quantity if supply.id != fm else fmq
             if sale_q <= supply_avail_q:
                 matched = True
             q_update = min(sale_q, supply_avail_q)
             sale_q -= q_update
-            if not first_supply_matched:
-                first_supply_matched = True
-                sale.first_matched_supply = supply
-                sale.fms_available_quantity = supply_avail_q
             supply_avail_q -= q_update
-            if supply.first_matched_sale_id is None:
-                supply.first_matched_sale_id = sale.id  # TODO: check
-            if sale_q == 0:
-                sale.last_matched_supply = supply
-                sale.lms_remaining_quantity = supply_avail_q
-            sale.total_net_profit += sale.price * q_update
-            sale.total_revenue += (sale.price - supply.price) * q_update
+            sale.total_net_profit += (sale.price - supply.price) * q_update
+            sale.total_revenue += sale.price * q_update
             sale.total_quantity += q_update
-            upd_supplies.append(supply)
-            if len(upd_supplies) > 1000:
-                Supply.objects.bulk_update(upd_supplies, fields=['first_matched_sale_id'])
-                upd_supplies = []
-        Supply.objects.bulk_update(upd_supplies, fields=['first_matched_sale_id'])
         prev_sale = sale
         upd_sales.append(sale)
         if len(upd_sales) > 1000:
@@ -62,96 +57,35 @@ def recalculate(sales, supplies, prev_sale, fm, fmq):
                 'total_net_profit',
                 'total_quantity',
                 'total_revenue',
-                'first_matched_supply',
-                'fms_available_quantity',
-                'last_matched_supply',
-                'lms_remaining_quantity',
             ])
             upd_sales = []
-        fm = supply.id
-        fmq = ...
     Sale.objects.bulk_update(upd_sales, fields=[
         'total_net_profit',
         'total_net_profit',
         'total_quantity',
         'total_revenue',
-        'first_matched_supply',
-        'fms_available_quantity',
-        'last_matched_supply',
-        'lms_remaining_quantity',
     ])
 
 
-def get_sales(barcode, dt_from=None):
-    kwargs = dict(barcode=barcode)
-    if dt_from:
-        kwargs['sale_time__gte'] = dt_from
-    all_sales = Sale.objects.filter(**kwargs).order_by('sale_time', 'id') \
+def get_sales(barcode):
+    all_sales = Sale.objects.filter(barcode=barcode).order_by('sale_time', 'id') \
         .iterator(chunk_size=1000)
     return all_sales
 
 
-def get_supplies(barcode, dt_from=None, id_from=None):
-    kwargs = dict(barcode=barcode)
-    args = []
-    if dt_from:
-        kwargs['supply_time__gte'] = dt_from
-    if id_from:
-        kwargs = {}
-        args = [Q(supply_time__gte=dt_from) | Q(supply_time=dt_from, id__gte=id_from)]
-    all_supplies = Supply.objects.filter(*args, **kwargs).order_by('supply_time', 'id') \
+def get_supplies(barcode):
+    all_supplies = Supply.objects.filter(barcode=barcode).order_by('supply_time', 'id') \
         .iterator(chunk_size=1000)
-    # msg = ''
-    # for sup in all_supplies:
-    #     msg += f'{sup.supply_time} '
-    # assert 0, msg
     return all_supplies
 
 
 def new_supply(new_sup):
-    sales = get_sales(new_sup.barcode, dt_from=new_sup.supply_time)
-    try:
-        sale = next(sales)
-    except StopIteration:
-        return
-    prev_sale = Sale.objects.filter(Q(sale_time__lt=sale.sale_time) | Q(sale_time=sale.sale_time, id__lt=sale.id)) \
-        .order_by('sale_time').last()
-    prev_supply = Supply.objects.filter(Q(supply_time__lt=new_sup.supply_time) | Q(supply_time=new_sup.supply_time, id__lt=new_sup.id)) \
-        .order_by('supply_time').last()
-    fm = fmq = None
-
-    if prev_supply is None:
-        supplies = get_supplies(new_sup.barcode)
-    else:
-        if new_sup.supply_time > sale.first_matched_supply and sale.first_matched_supply.supply_time:
-            supplies = get_supplies(new_sup.barcode, dt_from=sale.first_matched_supply.supply_time, id_from=sale.first_matched_supply.id)
-            fm = sale.first_matched_supply.id
-            fmq = sale.fms_available_quantity
-        else:
-            supplies = get_supplies(new_sup.barcode)
-    recalculate(chain([sale], sales), supplies, prev_sale, fm, fmq)
+    recalculate(get_sales(new_sup.barcode), get_supplies(new_sup.barcode), None, None)
 
 
 
 def new_sale(new_sale_):
-    sales = get_sales(new_sale_.barcode, dt_from=new_sale_.sale_time)
-    try:
-        sale = next(sales)
-    except StopIteration:
-        return
-    prev_sale = Sale.objects.filter(Q(sale_time__lt=sale.sale_time) | Q(sale_time=sale.sale_time, id__lt=sale.id)) \
-        .order_by('sale_time').last()
-    if prev_sale is None:
-        supplies = get_supplies(new_sale_.barcode)
-    else:
-        if prev_sale.last_matched_supply is None:
-            return
-        supplies = get_supplies(new_sale_.barcode,
-                                id_from=prev_sale.last_matched_supply.id,
-                                dt_from=prev_sale.last_matched_supply.supply_time)
-    recalculate(chain([sale], sales), supplies, prev_sale,
-                prev_sale.last_matched_supply.id if prev_sale and prev_sale.last_matched_supply else None,
-                prev_sale.lms_remaining_quantity if prev_sale and prev_sale.last_matched_supply else None)
+    recalculate(get_sales(new_sale_.barcode), get_supplies(new_sale_.barcode), None, None)
 
 
 class SaleViewSet(viewsets.ModelViewSet):
@@ -172,6 +106,41 @@ class SaleViewSet(viewsets.ModelViewSet):
         new_sale(serializer.instance)
         return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        recalculate(get_sales(instance.barcode), get_supplies(instance.barcode), None, None)
+        return Response(status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        recalculate(get_sales(instance.barcode), get_supplies(instance.barcode), None, None)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        barcode = request.query_params.get('barcode')
+        from_time = request.query_params.get('fromTime')
+        to_time = request.query_params.get('toTime')
+        make_kwargs(barcode, from_time, to_time, is_sale=True)
+        queryset = self.filter_queryset(self.get_queryset()).filter(**kwargs)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class SupplyViewSet(viewsets.ModelViewSet):
     queryset = Supply.objects.all()
@@ -191,6 +160,40 @@ class SupplyViewSet(viewsets.ModelViewSet):
         new_supply(serializer.instance)
         return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        recalculate(get_sales(instance.barcode), get_supplies(instance.barcode), None, None)
+        return Response(status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        recalculate(get_sales(instance.barcode), get_supplies(instance.barcode), None, None)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        barcode = request.query_params.get('barcode')
+        from_time = request.query_params.get('fromTime')
+        to_time = request.query_params.get('toTime')
+        make_kwargs(barcode, from_time, to_time, is_sale=False)
+        queryset = self.filter_queryset(self.get_queryset()).filter(**kwargs)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -205,20 +208,35 @@ def get_reports(request):
     try:
         from_time = timezone.datetime.strptime(from_time, '%Y-%m-%d %H:%M:%S')
         to_time = timezone.datetime.strptime(to_time, '%Y-%m-%d %H:%M:%S')
+        barcode = int(barcode)
     except ValueError:
         return JsonResponse({'error': 'Invalid datetime format'}, status=400)
 
-    # sales = Sale.objects.filter(barcode=barcode, sale_time__gte=from_time, sale_time__lte=to_time)
-    #
-    # quantity = sales.aggregate(Sum('quantity'))['quantity__sum'] or 0
-    # revenue = sales.aggregate(Sum('price'))['price__sum'] or 0
-    # net_profit = sales.aggregate(Sum('price') - Sum('supply__price'))['price__sum'] or 0
-    #
-    # response_data = {
-    #     'barcode': barcode,
-    #     'quantity': quantity,
-    #     'revenue': revenue,
-    #     'netProfit': net_profit
-    # }
+    gt = lt = None
 
-    return JsonResponse({'good': 'good'}, status=200)
+    gt_time = Sale.objects.filter(barcode=barcode, sale_time__lte=to_time).aggregate(Max('sale_time'))['sale_time__max']
+    lt_time = Sale.objects.filter(barcode=barcode, sale_time__lt=from_time).aggregate(Max('sale_time'))['sale_time__max']
+
+    if gt_time:
+        gt_id = Sale.objects.filter(barcode=barcode, sale_time=gt_time).aggregate(Max('id'))['id__max']
+        gt = Sale.objects.get(id=gt_id)
+    if lt_time:
+        lt_id = Sale.objects.filter(barcode=barcode, sale_time=lt_time).aggregate(Max('id'))['id__max']
+        lt = Sale.objects.get(id=lt_id)
+
+    rev = prof = quantity = 0
+    if gt:
+        rev += gt.total_revenue
+        prof += gt.total_net_profit
+        quantity += gt.total_quantity
+    if lt:
+        rev -= lt.total_revenue
+        prof -= lt.total_net_profit
+        quantity -= lt.total_quantity
+
+    return JsonResponse({
+        'barcode': barcode,
+        'revenue': rev,
+        'netProfit': prof,
+        'quantity': quantity,
+    }, status=200)
